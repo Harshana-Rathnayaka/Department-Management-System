9d1c8a5f9994b19d45d40e429bd1df51d795db8d
Harshana ratnayaka <huratnayaka@gmail.com>
2021-03-16 00:16:06 +0530

    inactivity logout changed to 30 minutes

--- timeout.js
+++ timeout.js
@@ -1,38 +1,40 @@
-$(document).ready((function () {
+$(document).ready(function () {
+  var timestamp = new Date();
+  sessionStorage.setItem("lastTimestamp", timestamp);
 
-    var timestamp = new Date();
+  function timeChecker() {
+    setInterval(function () {
+      var storedTimestamp = sessionStorage.getItem("lastTimestamp");
+      timeCompare(storedTimestamp);
+    }, 3000);
+  }
+
+  function timeCompare(timeString) {
+    var currentTime = new Date();
+    var pastTime = new Date(timeString);
+    var timeDifference = currentTime - pastTime;
+    var minutesPassed = Math.floor(timeDifference / 60000);
+
+    if (minutesPassed > 30) {
+      sessionStorage.removeItem("lastTimestamp");
+      window.location = "../logout.php?logout";
+      return false;
+    } else {
+      console.log(
+        currentTime +
+          " - " +
+          pastTime +
+          " - " +
+          minutesPassed +
+          " Minutes Passed"
+      );
+    }
+  }
+
+  $(document).mousemove(function () {
+    timestamp = new Date();
     sessionStorage.setItem("lastTimestamp", timestamp);
+  });
 
-    function timeChecker() {
-        setInterval(function () {
-            var storedTimestamp = sessionStorage.getItem("lastTimestamp");
-            timeCompare(storedTimestamp);
-        }, 3000);
-    }
-
-
-    function timeCompare(timeString) {
-        var currentTime = new Date();
-        var pastTime = new Date(timeString);
-        var timeDifference = currentTime - pastTime;
-        var minutesPassed = Math.floor((timeDifference / 60000));
-
-
-        if (minutesPassed > 10) {
-            sessionStorage.removeItem("lastTimestamp");
-            window.location = "../logout.php?logout";
-            return false;
-        } else {
-            console.log(currentTime + " - " + pastTime + " - " + minutesPassed + " Minutes Passed");
-        }
-    }
-
-
-    $(document).mousemove(function () {
-        timestamp = new Date();
-        sessionStorage.setItem("lastTimestamp", timestamp);
-    });
-
-    timeChecker();
-
-}));
+  timeChecker();
+});
